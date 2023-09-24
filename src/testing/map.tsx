8cac3055297cf7ddebbc774f006813af91200975
unknown <rafaelmasallo@gmail.com>
2023-09-24 12:20:47 +0800

    👤 chore: 📚 latest based on tutorial

--- src/testing/map.tsx
+++ src/testing/map.tsx
@@ -23,29 +23,56 @@ export const Map = () => {
         const playerImage = new Image();
         playerImage.src = playerDown;
 
-        mapImage.onload = () => {
-            
+        interface ISprite {
+            position: { x:number; y:number };
+            velocity: number;
+            image: HTMLImageElement;
         }
 
-        window.addEventListener('keydown', (e) => {
-            console.log();
-
-            switch (e.key) {
-                case 'w':
-                    break;
-                case 'a':
-                    break;
-                case 's':
-                    break;
-                case 'd':
-                    break;
+        class Sprite {
+            position: { x: number, y: number };
+            velocity: number = 0;
+            image: HTMLImageElement;
+    
+            constructor({position, velocity, image}: ISprite){
+                this.position = position;
+                this.image = image;
             }
+
+            draw() {
+                c?.drawImage(this.image, this.position.x, this.position.y);
+            }
+        }
+
+        const background = new Sprite({
+            position: {
+                x: 0,
+                y: 0
+            },
+            velocity: 0,
+            image: mapImage
         });
+
+        const keys = {
+            w: {
+                pressed: false
+            },
+            a: {
+                pressed: false
+            },
+            s: {
+                pressed: false
+            },
+            d: {
+                pressed: false
+            },
+        }
 
         const animate = () => {
             window.requestAnimationFrame(animate);
 
-            c.drawImage(mapImage, 0, 0);
+            // c.drawImage(mapImage, 0, 0);
+            background.draw();
 
             c.drawImage(
                 playerImage,
@@ -62,9 +89,70 @@ export const Map = () => {
                 playerImage.width / 4,
                 playerImage.height,
             );
+
+                // tODO
+                // https://www.youtube.com/watch?v=yP5DKzriqXA 2:00:00
+
+            if (keys.w.pressed && lastKey === 'w') {
+                background.position.y += 3
+            }
+            
+            if (keys.a.pressed && lastKey === 'a') {
+                background.position.x += 3;
+            }
+            
+            if (keys.s.pressed && lastKey === 's') {
+                background.position.y -= 3;
+            }
+
+            if (keys.d.pressed && lastKey === 'd') {
+                background.position.x -= 3;
+            }
         }
 
         animate();
+        
+        let lastKey = '';
+
+        window.addEventListener('keydown', (e) => {
+            console.log('keydown', e.key);
+            switch (e.key) {
+                case 'w':
+                    keys.w.pressed = true;
+                    lastKey = 'w';
+                    break;
+                case 'a':
+                    keys.a.pressed = true;
+                    lastKey = 'a';
+                    break;
+                case 's':
+                    keys.s.pressed = true;
+                    lastKey = 's';
+                    break;
+                case 'd':
+                    keys.d.pressed = true;
+                    lastKey = 'd';
+                    break;
+            }
+        });
+
+        window.addEventListener('keyup', (e) => {
+            console.log('keyup', e.key);
+            switch (e.key) {
+                case 'w':
+                    keys.w.pressed = false;
+                    break;
+                case 'a':
+                    keys.a.pressed = false;
+                    break;
+                case 's':
+                    keys.s.pressed = false;
+                    break;
+                case 'd':
+                    keys.d.pressed = false;
+                    break;
+            }
+        });
     }, []);
 
     return <div style={{backgroundColor: 'black'}}>
